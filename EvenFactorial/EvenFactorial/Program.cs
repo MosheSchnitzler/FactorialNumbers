acba4f2004cd04dcf62f0b56c7c042aefdcfb12b
Moshe Schnitzler <70296015+MosheSchnitzler@users.noreply.github.com>
2021-07-26 12:46:09 -0400

    Factorial

---
+++ EvenFactorial/EvenFactorial/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EvenFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number from 3 to 65");
+            var input = Console.ReadLine();
+            ulong num = Convert.ToUInt64(input);
+            ulong results = num;
+            if (results > 2 && results < 66)
+            {
+                while (num > 2)
+                {
+                    num--;
+                    results *= num;
+                }
+                Console.WriteLine($"The results is {results}.");
+            }
+            else
+            {
+                Console.WriteLine("You can only enter a number from 3 to 66");
+            }
+
+        }
+    }
+}
